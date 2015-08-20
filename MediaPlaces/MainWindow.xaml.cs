76e110460864a7724c0acd199e15154f533fdd48
Philipp Holzmann <PhilippHolzmann@yahoo.de>
2015-08-20 19:22:35 +0200

    add a new dialog to add new media

--- MediaPlaces/MainWindow.xaml.cs
+++ MediaPlaces/MainWindow.xaml.cs
@@ -21,35 +21,35 @@ namespace MediaPlaces
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection mSqlC = new SQLiteConnection();
         public MainWindow()
         {
             InitializeComponent();
-           
-            SQLiteConnection sqlC = new SQLiteConnection();
             
-            sqlC.ConnectionString = "DataSource=database.sqlite";
-            sqlC.Open();
-            
+            this.mSqlC.ConnectionString = "DataSource=database.sqlite";
+            this.mSqlC.Open();
+            updateTable();
+        }
+
+        public void updateTable()
+        {
             try
             {
-                SQLiteCommand command = new SQLiteCommand(sqlC);
-                command.CommandText = "CREATE TABLE IF NOT EXISTS media(mediaID INTEGER NOT NULL PRIMARY KEY, title VARCHAR(100) NOT NULL,place INTEGER NOT NULL)";
+                SQLiteCommand command = new SQLiteCommand(this.mSqlC);
+                command.CommandText = "CREATE TABLE IF NOT EXISTS media(mediaID INTEGER NOT NULL PRIMARY KEY, title TEXT NOT NULL,place INTEGER NOT NULL)";
                 command.ExecuteNonQuery();
-                command.CommandText = "CREATE TABLE IF NOT EXISTS place(placeID INTEGER NOT NULL PRIMARY KEY, placeName VARCHAR(100))";
+                command.CommandText = "CREATE TABLE IF NOT EXISTS place(placeID INTEGER NOT NULL PRIMARY KEY, placeName TEXT)";
                 command.ExecuteNonQuery();
                 command.CommandText = "SELECT media.title,place.placeName FROM media,place WHERE media.place = place.placeID";
                 SQLiteDataReader reader = command.ExecuteReader();
                 MediaList.ItemsSource = reader;
-                //MediaList.DataContext = reader;
             }
-            catch(SQLiteException e)
+            catch (SQLiteException e)
             {
                 MessageBox.Show(e.Message);
             }
-           
-            
-            //MediaList.DataContext = reader;
         }
+
         private void MenuItem_Open(object sender,RoutedEventArgs e)
         {
             //ToDo: open a file dialog and import a file
@@ -61,6 +61,14 @@ namespace MediaPlaces
         private void MenuItem_About(object sender,RoutedEventArgs e)
         {
             //toDo: open a about dialog
+        }
+
+        private void menuItemAddMedia_Click(object sender, RoutedEventArgs e)
+        {
+            //aaaa
+            addMediaDialog dlg = new addMediaDialog(this,this.mSqlC);
+            dlg.Show();
+            //updateTable();
         }
     }
 }
